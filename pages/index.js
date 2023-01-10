ba820d3cc82f422748bf258b697ba37a9fee7111
beboopr <linnets-router.09@icloud.com>
2023-01-10 00:45:10 -0500

    edits

--- pages/index.js
+++ pages/index.js
@@ -54,14 +54,14 @@ export default function Home() {
               Edgar Davila
             </h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
-              Developer and designer.
+              Full-Stack Developer and Engineer.
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
               Freelancer providing services for programming and design content
-              needs. Join me down below and let's get cracking!
+              needs. Join me down below and let's get you started!
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
-              <AiFillTwitterCircle />
+              <AiFillTwitterCircle/>
               <AiFillLinkedin />
               <AiFillYoutube />
             </div>
