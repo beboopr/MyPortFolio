68193716be5c2b4427c55177f964963d98341b7e
beboopr <linnets-router.09@icloud.com>
2023-01-11 17:44:53 -0500

    added bocacode project

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ import code from "../public/code.png";
 import design from "../public/design.png";
 import consulting from "../public/consulting.png";
 import Image from "next/image";
-import web1 from "../public/web1.png";
+import bocacode from "../public/bocacode.png";
 import web2 from "../public/web2.png";
 import web3 from "../public/web3.png";
 import web4 from "../public/web4.png";
@@ -158,7 +158,7 @@ export default function Home() {
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
-                src={web1}
+                src={bocacode}
               />
             </div>
             <div className="basis-1/3 flex-1">
