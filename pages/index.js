5f7604ce7f54f9668b1eddcda54a707e676c2d7c
beboopr <linnets-router.09@icloud.com>
2023-01-13 11:54:50 -0500

    added github link

--- pages/index.js
+++ pages/index.js
@@ -153,13 +153,14 @@ export default function Home() {
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
             <div className="basis-1/3 flex-1 ">
-              <Image
+              <Button href="https://github.com/bocacode/bocacode-intranet-typescript">
+                <Image
                 className="rounded-lg object-cover"
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
                 src={bocacode}
-              />
+              /> </Button>
             </div>
             <div className="basis-1/3 flex-1">
               <Image
