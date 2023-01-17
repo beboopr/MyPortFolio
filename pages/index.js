df15a8a3059f2ce669437f188ae093170ef7e3d1
beboopr <linnets-router.09@icloud.com>
2023-01-16 23:46:09 -0500

    added project pics and edited routes

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,11 @@ import design from "../public/design.png";
 import consulting from "../public/consulting.png";
 import Image from "next/image";
 import bocacode from "../public/bocacode.png";
-import web2 from "../public/web2.png";
-import web3 from "../public/web3.png";
-import web4 from "../public/web4.png";
-import web5 from "../public/web5.png";
-import web6 from "../public/web6.png";
+import pitchvote from "../public/pitchvote.png";
+import reactmovies from "../public/reactmovies.png";
+import united from "../public/united.png";
+// import web5 from "../public/web5.png";
+// import web6 from "../public/web6.png";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
@@ -163,33 +163,36 @@ export default function Home() {
               /> </Button>
             </div>
             <div className="basis-1/3 flex-1">
+              <Button href="https://github.com/beboopr/pitch-vote">
               <Image
                 className="rounded-lg object-cover"
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
-                src={web2}
-              />
+                src={pitchvote}
+              /> </Button>
             </div>
             <div className="basis-1/3 flex-1">
+              <Button href="https://github.com/beboopr/react-movies">
               <Image
                 className="rounded-lg object-cover"
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
-                src={web3}
-              />
+                src={reactmovies}
+              /> </Button>
             </div>
             <div className="basis-1/3 flex-1">
+              <Button href="https://github.com/beboopr/united-front">
               <Image
                 className="rounded-lg object-cover"
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
-                src={web4}
-              />
+                src={united}
+              /> </Button>
             </div>
-            <div className="basis-1/3 flex-1">
+            {/* <div className="basis-1/3 flex-1">
               <Image
                 className="rounded-lg object-cover"
                 width={"100%"}
@@ -206,7 +209,7 @@ export default function Home() {
                 layout="responsive"
                 src={web6}
               />
-            </div>
+            </div> */}
           </div>
         </section>
       </main>
