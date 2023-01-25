5ba27d1cdedb1de1842eaf652d3933de5034ecd0
beboopr <linnets-router.09@icloud.com>
2023-01-25 16:02:20 -0500

    fixed some grammar

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
       <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between dark:text-white">
-            <h1 className="font-burtons text-xl">my protfolio</h1>
+            <h1 className="font-burtons text-xl">my portfolio</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill
@@ -145,7 +145,7 @@ export default function Home() {
         </section>
         <section className="py-10">
           <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
+            <h3 className="font-burtons text-3xl py-1 dark:text-white ">Portfolio</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               Since the beginning of my journey as a freelance designer and
               developer, I've done remote work for
